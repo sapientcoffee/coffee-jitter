d3cd393ecd901f0d0faee3fc758f6a2d4fb0a5e9
SapientCoffee <rob.j.edwards@gmail.com>
2025-06-30 18:42:33 -0700

    feat: Add continuous memory leak to backend

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 type Pun struct {
@@ -19,19 +20,23 @@ var puns = []Pun{
 	{Pun: "I'm not addicted to coffee, we're just in a committed relationship."},
 }
 
-var leakySlice []Pun
+var leakySlice [][]byte
 
 func punHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	pun := puns[rand.Intn(len(puns))]
-	for i := 0; i < 1000000; i++ {
-		leakySlice = append(leakySlice, pun)
-	}
 	json.NewEncoder(w).Encode(pun)
 }
 
 func main() {
+	go func() {
+		for {
+			leakySlice = append(leakySlice, make([]byte, 10*1024*1024)) // 10MB
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
 	http.HandleFunc("/api/pun", punHandler)
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
